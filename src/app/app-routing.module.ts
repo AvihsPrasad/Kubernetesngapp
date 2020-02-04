32ca1074a5af6b68d7a5184a05783d47ebbb8c73
AvihsPrasad <agustya742@gmail.com>
2020-02-04 14:12:00 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LandingComponent } from "./landing/landing.component";
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'land', component: LandingComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
